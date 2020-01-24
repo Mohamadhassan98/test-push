43ef94ee461a78685e203e14040f9b55a090666a
Mohamadhassan <emohamadhassan@gmail.com>
2020-01-25 01:06:21 +0330

    It will push using 2019.2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, Test Jetbrains...
+          Learn React, Test Jetbrains' Clion
         </a>
       </header>
     </div>
